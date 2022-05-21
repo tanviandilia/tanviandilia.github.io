4848667f0d0f60b21e47bcda0cdb9771ffb12202
tanviandilia <105607109+tanviandilia@users.noreply.github.com>
2022-05-21 00:18:08 -0400

    changed story, added slideshow

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
